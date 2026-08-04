bf81db266b64ac261dda612df74cb2ce72edb784
sofie-kirk22 <133001261+sofie-kirk22@users.noreply.github.com>
2026-08-04 19:47:20 +0200

    migration: Add X, Y, Z coordinates to GameMoveEntity

---
+++ backend/DeathMahjong/Migrations/20260804174648_AddXYZToDB.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeathMahjong.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddXYZToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "X",
+                table: "GameMoves",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Y",
+                table: "GameMoves",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Z",
+                table: "GameMoves",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "X",
+                table: "GameMoves");
+
+            migrationBuilder.DropColumn(
+                name: "Y",
+                table: "GameMoves");
+
+            migrationBuilder.DropColumn(
+                name: "Z",
+                table: "GameMoves");
+        }
+    }
+}

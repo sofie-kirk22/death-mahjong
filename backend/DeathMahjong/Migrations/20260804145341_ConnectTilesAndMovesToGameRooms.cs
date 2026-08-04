180fc06cbd2f7edce9d05d011d9d28ef13564cd7
sofie-kirk22 <133001261+sofie-kirk22@users.noreply.github.com>
2026-08-04 16:54:01 +0200

    db migration for move

---
+++ backend/DeathMahjong/Migrations/20260804145341_ConnectTilesAndMovesToGameRooms.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeathMahjong.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConnectTilesAndMovesToGameRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "GameTiles",
+                newName: "TileType");
+
+            migrationBuilder.RenameColumn(
+                name: "Drinks",
+                table: "GameMoves",
+                newName: "Sips");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "GameMoves",
+                newName: "DrawnAt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlayerName",
+                table: "GameMoves",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "GameMoves",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlayerName",
+                table: "GameMoves");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "GameMoves");
+
+            migrationBuilder.RenameColumn(
+                name: "TileType",
+                table: "GameTiles",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "Sips",
+                table: "GameMoves",
+                newName: "Drinks");
+
+            migrationBuilder.RenameColumn(
+                name: "DrawnAt",
+                table: "GameMoves",
+                newName: "CreatedAt");
+        }
+    }
+}
